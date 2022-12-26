9c9f76024d298c8f751bb74e679b2fc4409fd76f
Vadim Tatarintcev <vadim.tatarintcev@gmail.com>
2022-12-26 16:29:50 +0300

    добавляет несколько блоков в разметку

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import { iosVhFix } from "./utils/ios-vh-fix";
 import { initModals } from "./modules/modals/init-modals";
 import { initVideo } from "./video";
+import { initSwiper } from "./swiper";
 
 // ---------------------------------
 
@@ -10,6 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   iosVhFix();
   initVideo();
+  initSwiper();
 
   // Modules
   // ---------------------------------
